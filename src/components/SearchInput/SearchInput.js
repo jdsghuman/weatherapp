768be43b6f562623c0fc4020a281424a4e790fb8
JD Ghuman <jdsghuman@gmail.com>
2019-02-03 16:18:26 -0600

    Implement autocomplete feature

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -2,32 +2,78 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 
+import Script from 'react-load-script';
+
 class Form extends Component {
   state = {
-    searchString: ''
+    city: '',
+    query: '',
+    locality: ''
   }
 
   clearInput = () => {
     this.setState({
-      searchString: ''
+      city: '',
+      query: ''
     })
   }
-  
+
   handleChange = (event) => {
     this.setState({
-      searchString: event.target.value
+      locality: event.target.value
     })
   }
 
   handleClick = () => {
     console.log('clicked');
-    this.props.dispatch({ type: 'FETCH_WEATHER', payload: this.state.searchString });
+    this.props.dispatch({ type: 'FETCH_WEATHER', payload: this.state.locality });
     this.clearInput();
   }
+
+  handleScriptLoad = e => {
+    // Declare Options For Autocomplete
+    var options = {
+      types: ['(cities)'],
+    };
+
+    // Initialize Google Autocomplete
+    /*global google*/ // To disable any eslint 'google not defined' errors
+    this.autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete'),
+      options,
+    );
+
+    // Fire Event when a suggested name is selected
+    this.autocomplete.addListener('place_changed', this.handlePlaceSelect);
+  }
+
+  handlePlaceSelect = e => {
+
+    // Extract City From Address Object
+    let addressObject = this.autocomplete.getPlace();
+    let address = addressObject.address_components;
+
+    document.getElementById('autocomplete').value = addressObject.formatted_address;
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState(
+        {
+          locality: addressObject.formatted_address,
+          city: address[0].long_name
+        }
+      );
+    }
+  }
+
   render() {
     return (
       <>
-        <Input onChange={this.handleChange} value={this.state.searchString} type="text" name="city" placeholder="Enter city" />
+        {/* <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYqcIHpBnvZKTUX6K-mT7DY-nze22Dcms&libraries=places"
+          onLoad={this.handleScriptLoad}
+        /> */}
+        <Input id="autocomplete" onChange={this.handleChange} value={this.state.locality} type="text" name="city" placeholder="Enter city" />
         <Button onClick={this.handleClick}>Add City</Button>
       </>
     )
@@ -43,6 +89,7 @@ const Input = styled.input`
   padding: 5px;
   outline: none;
   border-radius: 3px;
+  font-size: 1rem;
 `;
 
 const Button = styled.button`
@@ -50,10 +97,10 @@ const Button = styled.button`
   color: #ffffff;
   cursor: pointer;
   border: none;
-  padding: 7px 18px;
+  padding: 6px 18px;
   outline: none;
   border-radius: 3px;
-  font-size: .8rem;
+  font-size: 1rem;
   letter-spacing: 1.2px;
   :hover {
     background: #64beea;
